81a1de8e74c61d7c608864e70012ad7b580d301c
Konstantin Panov <k.panov@stud.elka.pw.edu.pl>
2022-06-01 17:13:57 +0200

    Fix scons

--- src/main.cpp
+++ src/main.cpp
@@ -14,22 +14,22 @@ using namespace parser;
 using namespace ast;
 
 // Commented out blocks do not work
-/* std::unique_ptr<Node> readAndParseStdin()
+std::unique_ptr<Node> readAndParseStdin()
 {
   Parser p(std::make_unique<Lexer>(std::cin));
   return p.parse();
-}*/
+}
 
 
 int main()
 {
-  /* std::cout << readAndParseStdin()->toString(); */
+  std::cout << readAndParseStdin()->toString();/* 
   Lexer l(std::cin);
   Token t;
   do
   {
     t = l.getToken();
     std::cout << t << " ";
-  } while(t.getType() != Token::Type::EndOfFile);
+  } while(t.getType() != Token::Type::EndOfFile); */
   return 0;
 }
